a2ae778b6366adf37f9864182e16836906511358
vagabun <greetingsfromdijkstra@yandex.ru>
2017-02-20 20:45:19 +0200

    try to make a string with list of all processes

--- read_stat.c
+++ read_stat.c
@@ -4,47 +4,57 @@
 #include <unistd.h>
 
 /* Eventually useless function
-void move_pointer (FILE *stat) {
-	int ptr = fgetc(stat);
+void move_pointer (FILE *status) {
+	int ptr = fgetc(status);
 	//Segmentation fault if make it recursive
-	while (ptr != '\n') ptr = fgetc(stat);
+	while (ptr != '\n') ptr = fgetc(status);
 	return;
 }
 */
 
+char *make_filename (int id) {
+	char *filename = malloc (sizeof(char) * 20);
+	sprintf(filename, "/proc/%d/status", id);
+	return filename;
+}
+
 int main () {
 
-	int pid = getpid();
-	char filename[50];
-	sprintf(filename, "/proc/%d/status", pid);
-	FILE *stat;
-	stat = fopen(filename, "r");
 	char key_1[] = "Name:";
 	char key_2[] = "PPid:";
 	char buf[50];	
-	char ppid[50];
-
-	while (stat != NULL) {
-		while (!feof(stat)) {
-			fscanf(stat, "%s", buf);
+	char tree[200];
+	int ppid;
+	int pid = getpid();
+	FILE *status;
+	status = fopen(make_filename(pid), "r");
+	
+	while (status != NULL) {
+		while (!feof(status)) {
+			fscanf(status, "%s", buf);
 			if (strcmp(buf, key_1) == 0) {
-				fscanf(stat, "%s", buf);
+				fscanf(status, "%s", buf);
+				strcat(tree, buf);
+				strcat(tree, " ");
 				printf("%s\n", buf);
 			}
 			else if (strcmp(buf, key_2) == 0) {
-				fscanf(stat, "%s", buf);
-				strcpy(ppid, buf);
-				printf("%s\n", buf);
+				fscanf(status, "%s", buf);
+
+				//strcat (tree, buf);
+				ppid = atoi(buf);
+				//strcpy(ppid, buf);
+				//printf("%d\n", ppid);
 			}
 		}
-		fclose(stat);
+		fclose(status);
 
-		sprintf(filename, "/proc/%s/status", ppid);
-		printf("%s\n", filename);
-		stat = fopen(filename, "r");
+		//printf("%s\n", filename);
+		status = fopen(make_filename(ppid), "r");
 	}
 		
 
-	if (stat == NULL) perror("Error opening file");
+	printf("%s\n", tree);
+	if (status == NULL) perror("Error opening file");
 	return 0;
 }
